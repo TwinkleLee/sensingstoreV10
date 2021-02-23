348ef5e019502563dccc794f4106ad3705c17fe4
TwinkleLee <624061283@qq.com>
2021-02-23 15:20:55 +0800

    typings.d.ts

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,6 +2,10 @@
 ///<reference path="../node_modules/abp-web-resources/Abp/Framework/scripts/libs/abp.jquery.d.ts"/>
 ///<reference path="../node_modules/abp-web-resources/Abp/Framework/scripts/libs/abp.signalr.d.ts"/>
 
+//V3
+///<reference path="../node_modules/moment/moment.d.ts"/>
+///<reference path="../node_modules/@types/moment-timezone/index.d.ts"/>
+
 // Typings reference file, see links for more information
 // https://github.com/typings/typings
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
